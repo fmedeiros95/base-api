4cdb46a0fdd829fed4eac396af04c855d8a1a568
Felipe Medeiros <felipe@demaria.com.br>
2022-12-22 22:20:19 -0300

    update mail.config.ts

--- src/config/mail.config.ts
+++ src/config/mail.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('mail', () => ({
 	host: process.env.MAIL_HOST,
 	port: Number(process.env.MAIL_PORT) || 587,
-	secure: process.env.MAIL_SSL === 'true',
+	secure: Boolean(process.env.MAIL_SSL === 'true'),
 	user: process.env.MAIL_USER,
 	password: process.env.MAIL_PASS,
 	defaultFrom: process.env.MAIL_FROM,
